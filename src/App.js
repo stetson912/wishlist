0082c5259b219b1efbf95e5951880124976fdb22
Trenton Stetson <trenton_stetson@stu.centralaz.edu>
2022-08-29 22:46:57 -0700

    env not working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
+//hooks
+import { useAuthContext } from './hooks/useAuthContext'
 //pages and components
 import Navbar from './components/Navbar'
 import Home from './pages/home/Home'
@@ -9,17 +10,20 @@ import Login from './pages/login/Login'
 import Dashboard from './pages/dashboard/Dashboard'
 
 function App() {
+  const { authIsReady } = useAuthContext()
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Dashboard" element={<Dashboard />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path='/login' element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Dashboard" element={<Dashboard />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
